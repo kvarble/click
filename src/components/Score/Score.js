7c7f75ea829a1db932040e9ec5a7116484fea0e3
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-07-02 17:32:10 -0500

    final

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -4,6 +4,9 @@ import "./Score.css";
 
 const Score = props => (
 <div className="score">
+
+  <p>Click on on each of the images once. The best score is 20!</p>
+
 <ul className="nav nav-pills" role="tablist">
   <li role="presentation" className="active"><p>Score<span className="badge one">{props.currentScore} </span></p></li> 
   <li role="presentation" className="active"><p>Best Score<span className="badge two">{props.bestScore} </span></p></li>
